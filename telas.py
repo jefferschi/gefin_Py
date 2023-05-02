7d2a7f51d8a32d1c147abe4478072c990038bd37
Jefferson Schittini <jefferschi@gmail.com>
2023-05-02 07:12:35 -0300

    observações

--- telas.py
+++ telas.py
@@ -3,6 +3,8 @@ from ttkbootstrap.constants import *
 
 from modelos import Cliente
 
+#  colocar o que seriam as views no Django, ver se precisará criar módulos.
+
 
 class ClienteCad:
     def __init__(self):
