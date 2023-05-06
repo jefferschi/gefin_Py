55912735886659d9bbbc146184956e3cf4080da3
Jefferson Schittini <jefferschi@gmail.com>
2023-05-06 11:41:03 -0300

    tela cliente

--- telas/cliente.py
+++ telas/cliente.py
@@ -16,7 +16,7 @@ class ClienteTelaCad:
     def __init__(self):
         self.janela = tb.Toplevel()
         self.janela.title("Cadastro de Clientes")
-        self.janela.geometry("600x450+55+55")
+        self.janela.geometry("600x450+70+70")
         self.janela.iconbitmap('img\icone.ico')
         self.janela.resizable(False,False)
         self.tela()
@@ -26,75 +26,79 @@ class ClienteTelaCad:
         # deixar todos os campos não editáveis ao abrir, exceto código
 
         # quadro para informações básicas do cliente
-        self.quadro = tb.Labelframe(self.janela, text='Dados do Cliente', bootstyle=INFO)
-        self.quadro.grid(row=0,column=0, padx=10, pady=5, ipadx=5, ipady=5, sticky=W)
+        self.qd_dados = tb.Labelframe(self.janela, text='Dados do Cliente', bootstyle=INFO)
+        self.qd_dados.grid(row=0,column=0, padx=10, pady=5, ipadx=5, ipady=5, sticky=W)
         
-        tb.Label(self.quadro, text='Código').grid(row=0, column=0, sticky=W, padx=5, pady=2)
-        self.codigo = tb.Entry(self.quadro, width=15)
-        self.codigo.grid(row=1, column=0, sticky=W, padx=5)
-        self.codigo.focus()
-
-        tb.Label(self.quadro, text='Nome').grid(row=0, column=1, sticky=W, padx=5, pady=2)
-        self.nome = tb.Entry(self.quadro, width=50)
-        self.nome.grid(row=1, column=1, sticky=W, padx=5)
-
-        ativo = IntVar(self.quadro, value=1) #variável para o check button campo Ativo 
-        self.ativo = tb.Checkbutton(self.quadro, text='Ativo', variable=ativo,
+        self.rt_cod = tb.Label(self.qd_dados, text='Código')
+        self.rt_cod.grid(row=0, column=0, sticky=W, padx=5, pady=2)
+        self.ent_codigo = tb.Entry(self.qd_dados, width=15)
+        self.ent_codigo.grid(row=1, column=0, sticky=W, padx=5)
+        self.ent_codigo.focus()
+        
+        self.rt_nome = tb.Label(self.qd_dados, text='Nome')
+        self.rt_nome.grid(row=0, column=1, sticky=W, padx=5, pady=2)
+        self.ent_nome = tb.Entry(self.qd_dados, width=50)
+        self.ent_nome.grid(row=1, column=1, sticky=W, padx=5, columnspan=2)
+        
+        self.v_ativo = IntVar(self.qd_dados, value=1) #variável para o check button campo Ativo 
+        self.cbt_ativo = tb.Checkbutton(self.qd_dados, text='Ativo', variable=self.v_ativo,
                                     onvalue=1, offvalue=0)
-        self.ativo.grid(row=0, column=2, padx=(0,5), sticky=E)
-        
-        
-        tb.Label(self.quadro, text='Pessoa').grid(row=2, column=0, sticky=W, padx=5, pady=2)
-        self.pessoa = tb.Combobox(self.quadro, values=['Física','Jurídica'], width=13)
-        self.pessoa.grid(row=3, column=0, sticky=W, padx=5)
-        
-        tb.Label(self.quadro, text='CPF/CNPJ').grid(row=2, column=1, sticky=W, padx=5, pady=2)
-        self.cpf_cnpj = tb.Entry(self.quadro, width=30)
-        self.cpf_cnpj.grid(row=3, column=1, sticky=W, padx=5)
+        self.cbt_ativo.grid(row=0, column=2, padx=5, sticky=E)
 
-        tb.Label(self.quadro, text='RG/IE').grid(row=2, column=2, sticky=W, padx=5, pady=2)
-        self.rg_ie = tb.Entry(self.quadro, width=15)
+        self.dic_pessoa = {'F':'Física','J':'Jurídica'} # para usar ao salvar no banco de dados
+        self.rt_pessoa = tb.Label(self.qd_dados, text='Pessoa')
+        self.rt_pessoa.grid(row=2, column=0, sticky=W, padx=5, pady=2)
+        self.cbx_pessoa = tb.Combobox(self.qd_dados, values=['Física','Jurídica'], width=13)
+        self.cbx_pessoa.grid(row=3, column=0, sticky=W, padx=5)
+        
+        self.rt_cpf_cnpj = tb.Label(self.qd_dados, text='CPF/CNPJ')
+        self.rt_cpf_cnpj.grid(row=2, column=1, sticky=W, padx=5, pady=2)
+        self.ent_cpf_cnpj = tb.Entry(self.qd_dados, width=30)
+        self.ent_cpf_cnpj.grid(row=3, column=1, sticky=W, padx=5)
+
+
+        self.rt_rg_ie = tb.Label(self.qd_dados, text='RG/IE')
+        self.rt_rg_ie.grid(row=2, column=2, sticky=W, padx=5, pady=2)
+        self.rg_ie = tb.Entry(self.qd_dados, width=15)
         self.rg_ie.grid(row=3, column=2, sticky=W, padx=5)
 
         # quadro 2 para informações de endereço e contato
-        self.quadro2 = tb.Labelframe(self.janela, text='Complementos', bootstyle=INFO)
-        self.quadro2.grid(row=1,column=0, padx=10, pady=5, ipadx=5, ipady=5, sticky=W)
-
-        # quadro para separar algumas informações dentro do quadro 2
-        self.q_contat = tb.Frame(self.quadro2)
-        self.q_contat.grid(row=0,column=0,sticky=W, padx=5, pady=2)
+        self.qd_compl = tb.Labelframe(self.janela, text='Complementos', bootstyle=INFO)
+        self.qd_compl.grid(row=1,column=0, padx=10, pady=5, ipadx=5, ipady=5, sticky=W)
         
-        tb.Label(self.q_contat, text='Telefone').grid(row=0,column=0, sticky=W, padx=5, pady=2)
-        self.tel = tb.Entry(self.q_contat,width=15)
-        self.tel.grid(row=1, column=0, sticky=W, padx=5)
+        self.rt_tel = tb.Label(self.qd_compl, text='Telefone')
+        self.rt_tel.grid(row=0,column=0, sticky=W, padx=5, pady=2)
+        self.ent_tel = tb.Entry(self.qd_compl,width=15)
+        self.ent_tel.grid(row=1, column=0, sticky=W, padx=5)
 
-        tb.Label(self.q_contat, text='E-mail').grid(row=0,column=1, sticky=W, padx=5, pady=2)
-        self.email = tb.Entry(self.q_contat,width=50)
-        self.email.grid(row=1, column=1, sticky=W, padx=5)
+        self.rt_email = tb.Label(self.qd_compl, text='E-mail')
+        self.rt_email.grid(row=0,column=1, sticky=W, padx=5, pady=2)
+        self.ent_email = tb.Entry(self.qd_compl,width=39)
+        self.ent_email.grid(row=1, column=1, sticky=W, padx=5, columnspan=2)
+
+        self.rt_end = tb.Label(self.qd_compl, text='Endereço')
+        self.rt_end.grid(row=2,column=0, sticky=W, padx=5, pady=2)
+        self.ent_end = tb.Entry(self.qd_compl,width=69)
+        self.ent_end.grid(row=3, column=0, sticky=W, padx=5, columnspan=3)
+
+        self.rt_bairro = tb.Label(self.qd_compl, text='Bairro')
+        self.rt_bairro.grid(row=4, column=0, sticky=W, padx=5, pady=2)
+        self.ent_bairro = tb.Entry(self.qd_compl, width=25)
+        self.ent_bairro.grid(row=5, column=0, sticky=W, padx=5)
+
+        self.rt_cidade = tb.Label(self.qd_compl, text='Cidade')
+        self.rt_cidade.grid(row=4, column=1, sticky=W, padx=5, pady=2)
+        self.ent_cidade = tb.Entry(self.qd_compl, width=25)
+        self.ent_cidade.grid(row=5, column=1, sticky=W, padx=5)
         
-        tb.Label(self.quadro2, text='Endereço').grid(row=1,column=0, sticky=W, padx=10, pady=2)
-        self.endereco = tb.Entry(self.quadro2,width=68)
-        self.endereco.grid(row=2, column=0, sticky=W, padx=10)
-
-        # quadro para complemento de endereço para separar informações dentro do quadro 2
-        self.qd_end_compl = tb.Frame(self.quadro2)
-        self.qd_end_compl.grid(row=3,column=0,sticky=W, padx=5, pady=2)
-
-        tb.Label(self.qd_end_compl, text='Bairro').grid(row=0, column=0, sticky=W, padx=5, pady=2)
-        self.bairro = tb.Entry(self.qd_end_compl, width=25)
-        self.bairro.grid(row=1, column=0, sticky=W, padx=5, pady=2)
-
-        tb.Label(self.qd_end_compl, text='Cidade').grid(row=0, column=1, sticky=W, padx=5, pady=2)
-        self.cidade = tb.Entry(self.qd_end_compl, width=25)
-        self.cidade.grid(row=1, column=1, sticky=W, padx=5, pady=2)
-
-        tb.Label(self.qd_end_compl, text='UF').grid(row=0, column=2, sticky=W, padx=5, pady=2)
-        self.uf = tb.Entry(self.qd_end_compl, width=4)
-        self.uf.grid(row=1, column=2, sticky=W, padx=5, pady=2)
-
+        self.rt_uf = tb.Label(self.qd_compl, text='UF')
+        self.rt_uf.grid(row=4, column=2, sticky=W, padx=5, pady=2)
+        self.uf = tb.Entry(self.qd_compl, width=4)
+        self.uf.grid(row=5, column=2, sticky=W, padx=5)
+        
         # quadro para os botões
         self.qd_bt = tb.Frame(self.janela)
-        self.qd_bt.grid(row=4,column=0,sticky=W, padx=5, pady=10)
+        self.qd_bt.grid(row=2,column=0,sticky=W, padx=5, pady=10)
 
         self.bt_inclui = tb.Button(self.qd_bt, text='Inclui', bootstyle=SUCCESS)
         self.bt_inclui.pack(side=LEFT, padx=(5,0))
@@ -104,7 +108,6 @@ class ClienteTelaCad:
 
         self.bt_busca = tb.Button(self.qd_bt, text='Busca', bootstyle=WARNING)
         self.bt_busca.pack(side=LEFT, padx=(5,0))
-
     
         
 """        
--- utilitar/tabelas.py
+++ utilitar/tabelas.py
@@ -27,7 +27,7 @@ class TabelaDados:
                 CREATE TABLE IF NOT EXISTS clientes (
                     codigo INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
                     nome VARCHAR(100) NOT NULL,
-                    pessoa CHAR(10),
+                    pessoa CHAR(1),
                     cnpj_cpf VARCHAR(14) UNIQUE,
                     rg_ie VARCHAR(15),
                     telefone TEXT,
