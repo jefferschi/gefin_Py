0d2cacbf24441f6832caf956a167e543cf7cb307
Jefferson Schittini <jefferschi@gmail.com>
2023-05-11 11:16:28 -0300

    instanciar cliente

--- models/clientes.py
+++ models/clientes.py
@@ -1,9 +1,55 @@
 import ttkbootstrap as tb
 from ttkbootstrap.constants import *
 
-# colocar as classes que estruturam os dados e conectam com o banco de dados
-# criar o banco de dados diretamento no SQLiteStudio ou similar, não criar no código python
+"""  Orientações para criação e uso da classe
+
+# Exemplo: atribuição direta dos atributos individuais
+# self.codigo = self.atributos['codigo']
+# self.nome = self.atributos['nome']
+# self.cpf = self.atributos['cpf']
+# ...
+
+# Ou, se preferir, pode acessar os atributos diretamente pelo dicionário:
+# Exemplo: self.atributos['codigo'], self.atributos['nome'], self.atributos['cpf'], ...
+"""
 
 class Cliente:
-    def __init__(self):
-        pass
+   
+    def __init__(self, *args):
+        self.atributos = {
+            'codigo': None,
+            'nome': None,
+            'cnpj_cpf': None,
+            'rg_ie': None,
+            'telefone': None,
+            'email': None,
+            'endereco': None,
+            'bairro': None,
+            'cidade': None,
+            'uf': None,
+            'ativo':None
+        }
+
+        if len(args) > len(self.atributos):
+            raise ValueError("Número inválido de argumentos")
+
+        for i, arg in enumerate(args):
+            atributo = list(self.atributos.keys())[i]
+            self.atributos[atributo] = arg
+
+
+    def incluir(self):
+        # teste para instanciar a classe
+
+        cod = input('cod: ')
+        nome = input('nome: ')
+        cpf = input('cpf: ')
+
+        cliente = Cliente(cod,nome,cpf)
+
+        print(cliente.atributos['codigo'])
+        print(cliente.atributos['nome'])
+        print(cliente.atributos['cnpj_cpf'])
+
+c = Cliente()
+c.incluir()
